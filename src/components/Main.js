ba39ba72325b70f080c3b2dedd4e1a8e04ec5044
xiaochenggit <707996290@qq.com>
2017-06-05 18:03:16 +0800

    判定范围，随机设置位置，随机设置角度
    判定范围，随机设置位置，随机设置角度

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,89 @@ import ImgFigure from './imgFigure';
 
 // 图片数据 json
 
-let imageDatas = require('json!../data/imageData.json');
+// let imageDatas = require('json!../data/imageData.json');
+let imageDatas  = [
+  {
+    fileName: '1.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '2.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '3.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '4.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '5.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '6.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '7.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '8.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '9.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '10.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '11.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '12.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '13.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '14.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '15.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  },
+  {
+    fileName: '16.jpg',
+    title: 'Happy time',
+    desc: 'This is a worderful scenery photograph.'
+  }
+]
 // let yeomanImage = require('../images/yeoman.png');
 
 /**
@@ -26,16 +108,181 @@ imageDatas = (function(imageDataArr) {
 })(imageDatas)
 
 class AppComponent extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      Constant: {
+        // 中心图片的位置
+        centerPos: {
+          left: 0,
+          top: 0
+        },
+        // 水平方向的取值范围
+        hPosRange: {
+          leftSecX: [0, 0],
+          rightSecX: [0, 0],
+          y: [0, 0]
+        },
+        // 垂直方向的取值范围
+        vPosRange: {
+          x: [0, 0],
+          topY: [0, 0]
+        }
+      },
+      imgsArrangeArr: [
+      // 位置信息
+      // {
+      //   pos: {
+      //     left : 0,
+      //     right: 0
+      //   },
+      //   rotate: 12
+      // }
+      ]
+    }
+    this.reArrAange = this.reArrAange.bind(this);
+    this.getRangeRandom = this.getRangeRandom.bind(this);
+  }
+
   componentDidMount() {
-  	console.log(imageDatas);
+  	// 拿到舞台的大小
+
+    let stage = document.getElementById('stage');
+    let stageW = stage.scrollWidth;
+    let stageH = stage.scrollHeight;
+    let halfStageW = Math.ceil(stageW / 2);
+    let halfStageH = Math.ceil(stageH / 2);
+    // 拿到 imgFigure
+    
+    let imgFigureDom = document.getElementById('imgFigure0');
+    let imgW = imgFigureDom.scrollWidth;
+    let imgH = imgFigureDom.scrollHeight;
+    let halfImgW = Math.ceil(imgW / 2);
+    let halfImgH = Math.ceil(imgH / 2);
+    const Constant = {
+        // 获得中心图片的位置
+        centerPos: {
+          left:  halfStageW - halfImgW,
+          top: halfStageH - halfImgH
+        },
+        // 左右侧位置范围
+        hPosRange: {
+          leftSecX: [-halfImgW, halfStageW - halfImgW * 3],
+          rightSecX: [halfStageW + halfImgW, stageW - halfImgW],
+          y: [-halfImgH, stageH - halfImgH]
+        },
+        // 图片正上方区域
+        vPosRange: {
+          topY: [-halfImgH, halfStageH - halfImgH * 3],
+          x: [halfStageW - imgW, halfStageW]
+        }
+      }
+    // 默认中心图片为第一个
+    var imgsArrangeArr = this.state.imgsArrangeArr;
+    imageDatas.forEach((element, index) => {
+      if (!imgsArrangeArr[index]) {
+        imgsArrangeArr[index] = {
+          pos:{
+            left: 0,
+            top: 0
+          },
+          rotate: 0
+        }
+      }
+    });
+    this.setState({ imgsArrangeArr });
+    this.setState({Constant}, () => {
+        this.reArrAange(0)
+    })
+  }
+
+  /**
+   * [reArrAange 设置中心图片]
+   * @param  {[Number]} centerIndex [中心图片的 index]
+   * @return {[type]}             [description]
+   */
+  reArrAange(centerIndex) {
+    var self = this;
+    var imgsArrangeArr = this.state.imgsArrangeArr,
+    Constant = this.state.Constant,
+    centerPos = Constant.centerPos,
+    hPosRange = Constant.hPosRange,
+    vPosRange = Constant.vPosRange,
+    hPosRangeLeftSecX = hPosRange.leftSecX,
+    hPosRangeRightSecX = hPosRange.rightSecX,
+    hPosRangeY = hPosRange.y,
+    vPosRangeTopY = vPosRange.topY,
+    vPosRangeX = vPosRange.x,
+
+
+    // 上侧区域 取1个或者不取
+    imgArrangeTopArr = [],
+    topImgNum = Math.ceil(Math.random() * 2),
+    topImgSpliceIndex = 0,
+
+    // 中心图片
+    imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
+    imgsArrangeCenterArr[0]= {
+      pos: centerPos,
+      rotate: 0
+    };
+    // 上侧区域图片状态
+    topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
+    imgArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+    // 布局上侧区域图片
+    imgArrangeTopArr.forEach(function (value, index) {
+      imgArrangeTopArr[index] = {
+        pos: {
+          top: self.getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+          left: self.getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+        },
+        rotate: self.get30Rotate()
+      }
+    });
+    // 左右两侧的图片
+   
+    for (var i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
+      var hPosRangeLORX = null;
+      // 前半部分布局在左侧, 后半部分布局在右侧
+      if (i < k) {
+        hPosRangeLORX = hPosRangeLeftSecX;
+      } else {
+        hPosRangeLORX = hPosRangeRightSecX;
+      }
+      imgsArrangeArr[i] = {
+        pos: {
+          left: self.getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1]),
+          top: self.getRangeRandom(hPosRangeY[0], hPosRangeY[1])
+        },
+        rotate: self.get30Rotate()
+      }
+    }
+
+    // 连接数组并改变它
+    var imgsArrangeArr = imgsArrangeCenterArr.concat(imgArrangeTopArr).concat(imgsArrangeArr);
+    this.setState({ imgsArrangeArr });
+  }
+
+  /**
+   * [getRangeRandom 区间随机数]
+   * @param  {[Number]} low  [小值]
+   * @param  {[Number]} high [大值]
+   * @return {[type]}      [description]
+   */
+  getRangeRandom(low, high) {
+    return Math.ceil(Math.random() * (high - low) + low);
+  }
+  get30Rotate() {
+    return (Math.random() > 0.5 ? '' : '-') + Math.round(Math.random() * 30)
   }
   render() {
     return (
       <div className="index">
-        <section className="stage">
+        <section className="stage" ref={(stage) => this.stage = stage } id="stage">
         	<section className="img-sec">
         		{
-        			imageDatas.map((item, index) => <ImgFigure data={item} key={index}></ImgFigure>)
+        			imageDatas.map((item, index) => <ImgFigure data={item} key={index} id={'imgFigure' + index} range={this.state.imgsArrangeArr[index]}></ImgFigure>)
         		}
         	</section>
         	<section className="controller-nav">
--- src/components/imgFigure.js
+++ src/components/imgFigure.js
@@ -1,11 +1,25 @@
 import React , { Component } from 'react';
 
 class ImgFigure extends Component {
+	constructor(){
+		super();
+		this.state = {
+			style: {
+
+			}
+		}
+	}
 	render() {
+		if (this.props.range) {
+			this.state.style = this.props.range.pos;
+			['-moz-','-ms-','-webkit',''].forEach((value) => {
+				this.state.style[value + 'transform'] = 'rotate(' + this.props.range.rotate + 'deg)';
+			});
+		}
 		return (
-			<figure className="img-figure">
+			<figure className="img-figure" id ={this.props.id} style={this.state.style}>
 				<img src={this.props.data.imageURL}
-					 alt={this.props.data.title}/>
+					 alt={this.props.data.title} />
 				<figcaption>
 					<h2 className="img-title">{this.props.data.title}</h2>
 				</figcaption>
